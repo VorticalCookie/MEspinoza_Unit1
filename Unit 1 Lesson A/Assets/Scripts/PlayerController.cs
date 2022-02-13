fc65832207a6e321985b5239f381043b16255b8d
VorticalCookie <92548003+VorticalCookie@users.noreply.github.com>
2022-02-13 15:47:47 -0800

    Lesson D

--- Unit 1 Lesson A/Assets/Scripts/PlayerController.cs
+++ Unit 1 Lesson A/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    // rate of forward movement and horizontal movement
     public float speed = 13.0f;
+    public float turnSpeed = 13.0f;
+
+    private float horizontalInput;
+    private float verticalInput;
 
     void Start()
     {
@@ -15,9 +19,14 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
         Debug.Log(Time.deltaTime);
 
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        //turning vehicle script
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
     }
 }
